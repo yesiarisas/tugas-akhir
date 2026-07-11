093d966da829630f1c689807fa1f832075049d30
Agung Bahtiar <agungbahtiar050@gmail.com>
2026-07-11 09:48:27 +0700

    Add /debug-host route to expose request info

--- routes/web.php
+++ routes/web.php
@@ -8,6 +8,16 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 */
 
+
+Route::get('/debug-host', function () {
+    return [
+        'host' => request()->getHost(),
+        'scheme' => request()->getScheme(),
+        'is_secure' => request()->isSecure(),
+        'trusted_proxies' => request()->getTrustedProxies(),
+    ];
+});
+
 // Halaman Utama
 Route::get('/', function () {
     return view('welcome');
@@ -15,5 +25,15 @@ Route::get('/', function () {
 
 // Pastikan semua alamat lain juga lari ke 'welcome' agar ditangani oleh Vue
 Route::get('/{any}', function () {
-    return view('welcome'); 
+    return view('welcome');
 })->where('any', '.*');
+
+
+Route::get('/debug-host', function () {
+    return [
+        'host' => request()->getHost(),
+        'scheme' => request()->getScheme(),
+        'is_secure' => request()->isSecure(),
+        'trusted_proxies' => request()->getTrustedProxies(),
+    ];
+});
